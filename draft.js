f546dbc6dbb13c095980293910feefffde67ffa3
lionffl <60923432+lionffl@users.noreply.github.com>
2021-08-28 09:53:02 -0300

    draft update

--- draft.js
+++ draft.js
@@ -324,33 +324,3 @@ const author = comments.find((findAuthor) => findAuthor.author === 'Lion');
 
 console.log(author);
 
-const scores = [100000,9,3,10,2,20];
-
-function breakingRecords(scores) {
-    let minNumber = scores[0];
-    let maxNumber = scores[0];
-    let maxCounter = 0;
-    let minCounter = 0;
-    
-    for (let i = 1 ; i < scores.length ; i++) {
-
-        if (scores[i] === maxNumber) {
-            maxCounter = maxCounter;
-        }
-         if (scores[i] === minNumber) {
-            minCounter = minCounter;
-        }
-        if (scores[i] > maxNumber) {
-            maxNumber = scores[i];
-            maxCounter = maxCounter + 1;
-        }
-        if (scores[i] < minNumber) {
-            minNumber = scores[i];
-            minCounter = minCounter + 1;
-        }
-    }
-    const breakingRecordsAnswer = [maxCounter,minCounter];
-    console.log(breakingRecordsAnswer);
-}
-
-breakingRecords(scores);
