ef22d8737e4a84abca7e45b7afceddce22c9d0db
lionffl <60923432+lionffl@users.noreply.github.com>
2021-08-31 10:30:01 -0300

    check invalid parameters in calcEst() added

--- javascript/projects/js/first.js
+++ javascript/projects/js/first.js
@@ -8,7 +8,11 @@ function calcEst() {
     let economicCorrection = document.getElementsByClassName('economic-correction')[0].value;
     let sourceFactor = sourcePayment / ((sourceExtension * sourceWidth) / 10000);
     let result = economicCorrection * (sourceFactor * ((projectExtension * projectWidth) / 10000));
-    result = `R$${result.toFixed(2)}`;
-    document.getElementsByClassName('result')[0].innerText = result;
-    document.getElementsByClassName('answer')[0].innerText = `Considerando uma Linha de Transmissão de ${sourceExtension/1000}km de extensão e sua faixa de servidão de ${sourceWidth}metros indenizada em ${sourceYear} por R$${sourcePayment}, chegamos ao valor médio por hectare de R$${sourceFactor.toFixed(2)}. Portanto, para uma Linha de Transmissão de ${projectExtension/1000}km de extensão e ${projectWidth}metros de faixa de servidão, chegamos ao valor de ${result} após aplicar o índice de correção de ${economicCorrection}.`;
+    if (result) {
+        result = `R$${result.toFixed(2)}`;
+        document.getElementsByClassName('result')[0].innerText = result;
+        document.getElementsByClassName('answer')[0].innerText = `Considerando uma Linha de Transmissão de ${sourceExtension/1000}km de extensão e sua faixa de servidão de ${sourceWidth}metros indenizada em ${sourceYear} por R$${sourcePayment}, chegamos ao valor médio por hectare de R$${sourceFactor.toFixed(2)}. Portanto, para uma Linha de Transmissão de ${projectExtension/1000}km de extensão e ${projectWidth}metros de faixa de servidão, chegamos ao valor de ${result} após aplicar o índice de correção de ${economicCorrection}.`;
+    } else {
+        alert('Todos os campos precisam ser preenchidos corretamente!');
+    }
 }
